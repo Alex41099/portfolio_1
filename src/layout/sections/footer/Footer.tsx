b18b83bc0c26c679421be9d49a8dfecfa3f78ea3
Alex41099 <nevazhnoalikhan@gmail.com>
2024-06-25 16:41:03 +0500

    module 3 - Refactoring Footer

--- src/layout/sections/footer/Footer.tsx
+++ src/layout/sections/footer/Footer.tsx
@@ -4,73 +4,54 @@ import {Icon} from "../../../components/icon/Icon";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {theme} from "../../../styles/Theme";
 import {font} from "../../../styles/Common";
+import { S } from './Footer_Styles';
+
+const styledA = [
+    {
+        href: "#",
+        width: "21",
+        height: "21",
+        viewBox: "0 0 21 21",
+        iconId: "instagramSvg"
+    },
+    {
+        href: "#",
+        width: "21",
+        height: "21",
+        viewBox: "0 0 21 21",
+        iconId: "telegramSvg"
+    },
+    {
+        href: "#",
+        width: "21",
+        height: "21",
+        viewBox: "0 0 21 21",
+        iconId: "vkSvg"
+    },
+    {
+        href: "#",
+        width: "21",
+        height: "21",
+        viewBox: "0 0 21 21",
+        iconId: "linkedinSvg"
+    }
+]
 
 export const Footer = () => {
     return (
-        <StyledFooter>
+        <S.StyledFooter>
             <FlexWrapper direction={"column"} align={"center"}>
-                <StyledH2>Svetlana</StyledH2>
-                <StyledUl>
-                    <li>
-                        <StyledA href="#"><Icon width={"21"} height={"21"} viewBox={"0 0 21 21"} iconId={"instagramSvg"}/></StyledA>
-                    </li>
-                    <li>
-                        <StyledA href="#"><Icon width={"21"} height={"21"} viewBox={"0 0 21 21"} iconId={"telegramSvg"}/></StyledA>
-                    </li>
-                    <li>
-                        <StyledA href="#"><Icon width={"21"} height={"21"} viewBox={"0 0 21 21"} iconId={"vkSvg"}/></StyledA>
-                    </li>
-                    <li>
-                        <StyledA href="#"><Icon width={"21"} height={"21"} viewBox={"0 0 21 21"} iconId={"linkedinSvg"}/></StyledA>
-                    </li>
-                </StyledUl>
-                <StyledCopyright>© 2023 Svetlana Dyablo, All Rights Reserved.</StyledCopyright>
+                <S.StyledH2>Svetlana</S.StyledH2>
+                <S.StyledUl>
+                    {styledA.map((s, index) => {
+                        return <li>
+                            <S.StyledA href={s.href}><Icon width={s.width} height={s.height} viewBox={s.viewBox}
+                                                    iconId={s.iconId}/></S.StyledA>
+                            </li>})}
+                </S.StyledUl>
+                <S.StyledCopyright>© 2023 Svetlana Dyablo, All Rights Reserved.</S.StyledCopyright>
             </FlexWrapper>
-        </StyledFooter>
+        </S.StyledFooter>
     );
 };
 
-const StyledFooter = styled.footer`
-    padding: 40px 0 40px;
-    background-color: ${theme.color.primaryBg};
-    
-`
-
-const StyledH2 = styled.h2`
-    ${font({family: '"Josefin Sans", sans-serif;', weight: 700, Fmax: 22, Fmin: 16})}
-    
-    letter-spacing: 3px;
-`
-
-const StyledUl = styled.ul`
-    display: flex;
-    padding: 0 0;
-    margin: 30px 0 30px;
-    gap: 20px;
-
-`
-
-const StyledA = styled.a`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 35px;
-    height: 35px;
-    border-radius: 25px;
-    background: rgba(255, 255, 255, 0.10);
-    color: ${theme.color.accent};
-    
-    &:hover {
-        background-color: ${theme.color.accent};
-        color: ${theme.color.secondaryBg};
-        transform: translateY(-4px);
-    }
-    
-`
-
-const StyledCopyright = styled.span`
-    color: rgba(255, 255, 255, 0.50);
-    font-size: 12px;
-    font-weight: 400;
-    
-`
