92a9e02f1ccab91ffb283523c5ef032c35176df0
Alex41099 <nevazhnoalikhan@gmail.com>
2024-06-25 16:48:25 +0500

    module 3 - Refactoring Works and Work

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -4,33 +4,14 @@ import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Work} from "./Work";
 import photoProj1 from "../../../accets/images/proj-1.webp"
 import photoProj2 from "../../../accets/images/proj-2.webp"
-import styled from "styled-components";
-import {SectionTitle, StyledH2} from "../../../components/SectionTitle";
+import {SectionTitle} from "../../../components/SectionTitle";
 import {TabMenu} from "../../tabmenu/TabMenu";
 import {Container} from '../../../components/Container';
-import {theme} from "../../../styles/Theme";
 import { S } from './Works_Styles';
 
 const worksItems = ["ALL", "LANDING PAGE", "REACT", "SPA"]
 
-const work = [
-    {
-        photoProj: photoProj1,
-        title: "Social Network",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod " +
-            "tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit" +
-            " amet, consectetur adipisicing elit."
-    },
-    {
-        photoProj: photoProj2,
-        title: "Timer",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod " +
-            "tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit " +
-            "amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim "
-    }
-]
-
-export const Works = () => {
+export const Works: React.FC = () => {
     return (
         <S.StyledSection>
             <Container>
@@ -38,9 +19,16 @@ export const Works = () => {
                 <TabMenu array={worksItems}/>
 
                 <FlexWrapper justify={"space-between"} align={"flex-start"}>
-                    {work.map((s, index) => {
-                        return <Work photoProj={s.photoProj} title={s.title} description={s.description}/>
-                    })}
+                    <Work photoProj={photoProj1}
+                          title={"Social Network"}
+                          description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod " +
+                              "tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit" +
+                              " amet, consectetur adipisicing elit."}/>
+                    <Work photoProj={photoProj2}
+                          title={"Timer"}
+                          description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod " +
+                              "tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit " +
+                              "amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim "}/>
                 </FlexWrapper>
             </Container>
         </S.StyledSection>
