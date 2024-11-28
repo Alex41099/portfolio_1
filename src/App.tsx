0896c59d81b057a96078090061cec1056f06769e
Alex41099 <nevazhnoalikhan@gmail.com>
2024-11-28 08:15:15 +0500

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import {GoTopBtn} from "./components/goTopBtn/GoTopBtn";
 
 function App() {
     return (
+        // App
         <div className="App">
             <Particle/>
             <Header/>
